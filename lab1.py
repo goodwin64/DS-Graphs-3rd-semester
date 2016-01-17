1ffa06fae9500c2a5b381cb427724025626e843e
Max Donchenko <wereya2@gmail.com>
2016-01-18 00:16:22 +0200

    Processing the adjacency matrix via Floyd-Warshall algorithm was added. Also some little bugs was fixed.

--- lab1.py
+++ lab1.py
@@ -12,9 +12,22 @@ def countEdges(M):
                 edges += 1
     return edges
 
+def readline(file):
+    '''In case when first line contains <string>-info about matrix'''
+    char = file.read(1)
+    chars = [char]
+    while char != '\n':
+        char = file.read(1)
+        chars.append(char)
+        
+    return str().join(chars[:-1])
+    
+
 def readFromAdjacencyMatrix(G=Graph()):
     Matrix = []
     f = open('m1.txt', 'r')
+
+    # line-by-line reading from file and casting to <integer> type
     for line in f:
         Matrix.append( list( map( int, line.split())) )
 
@@ -27,17 +40,16 @@ def readFromAdjacencyMatrix(G=Graph()):
     return G, Matrix
 
 def writeToIncidenceMatrix(G, M1):
-    '''M1 - матрица смежности, которую мы читаем с файла
-       M2 - матрица инцидентности, которую мы создаем и записываем в новый файл
+    '''M1 - adjacency matrix which we read from the file
+       M2 - incidence matrix which we create and write to a new file
     '''
     edges = countEdges(M1)
-
     M2 = [[0 for i in range(edges)] for j in range(len(G.vertList))]
                 
     f = open('m2.txt', 'w')
     c = 0
-    for i in range(0, len(M1)):
-        for j in range(0, i+1):
+    for i in range(len(M1)):
+        for j in range(i+1):
             if M1[i][j] == 1:
                 M2[i][c] = 1
                 M2[j][c] = 1
@@ -45,11 +57,52 @@ def writeToIncidenceMatrix(G, M1):
 
     for row in M2:
         for cell in row:
-            f.write("%d " % cell)
+            f.write("%7d ".format(cell))
         f.write("\n")
 
     f.close()
     return M2
 
+def writeToDistanceMatrix(G, M1):
+    '''M1 - adjacency matrix which we read from the file
+       M3 - distance matrix which we create and write to a new file
+    '''
+    INF = 99 # some big number as kind of infinity
+    M3 = [[row[i] for i in range(len(M1))] for row in M1] # M3 is copy of M1
+
+    # Cells with 0-value except diagonal are init by infinity or big number
+    for i in range(len(M3)):
+        for j in range(len(M3)):
+            if M3[i][j] == 0:
+                M3[i][j] = INF
+
+    # Initializing diagonal with zeros
+    for i in range(len(M3)):
+        for j in range(len(M3)):
+            if i == j:
+                M3[i][j] = 0
+
+    # Floyd–Warshall algorithm
+    for k in range(len(M3)):
+        for i in range(len(M3)):
+            for j in range(len(M3)):
+                M3[i][j] = min(M3[i][j], M3[i][k] + M3[k][j])
+
+    f = open('m2.txt', 'w')
+
+    # From matrix to .txt-file
+    for row in M3:
+        for cell in row:
+            f.write("{0:4d}".format(cell))
+        f.write("\n")
+
+    f.close()
+    return M3
+    
+
 G, M1 = readFromAdjacencyMatrix()
 M2 = writeToIncidenceMatrix(G, M1)
+M3 = writeToDistanceMatrix(G, M1)
+##prettyPrint(M1)
+##prettyPrint(M2)
+##prettyPrint(M3)
