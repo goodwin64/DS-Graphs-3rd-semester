c2499842c6cb23d7f2db87b2a1e438d6de1facfb
Max Donchenko <wereya2@gmail.com>
2016-01-25 22:04:31 +0200

    Changed lab3 variant 3

--- Lab 3/lab3_var3_without_maxflow.py
+++ Lab 3/lab3_var3_without_maxflow.py
@@ -1,69 +1,77 @@
-class Vertex:
+# author = "Max Donchenko (https://github.com/goodwin64)"
 
-    def __init__(self, name, value=1, connections={}):
-        self.name = name
-        self.value = value
-        self.connections = connections
-
-    def __repr__(self):
-        return "%s(%d)" % (self.name, self.value)
+from random import randint
 
 
-class FlowNetwork:
-
-    def __init__(self):
-        self.groups_to_add = set()
-        self.full_rooms = set()
-
-    def add_vertex(self, vertex):
-        if vertex.name[0] == "g":
-            self.groups_to_add.add(vertex)
-        if vertex.name[0] == "s":
-            self.full_rooms.add(vertex)
-        return vertex
+def add_vertex(name, value, some_dict):
+    if name[0] == "g":
+        some_dict[name] = value
+    elif name[0] == "r":
+        some_dict[name] = value
 
 
-def min_but_more_than(room_set, group_size):
-    suitable_room = max(room_set, key=lambda x: x.value)
-    for room in room_set:
-        if room.value >= group_size and room.value < suitable_room.value:
-            suitable_room = room
-    return suitable_room
+def min_but_more_than(rooms_dict, group_size):
+    suitable_room_name = max(rooms_dict, key=lambda x: rooms_dict[x])
+    for room_name in list(rooms_dict.keys()):
+        if rooms_dict[suitable_room_name] > rooms_dict[room_name] >= group_size:
+            suitable_room_name = room_name
+    return suitable_room_name
 
-g = FlowNetwork()
 
-groups_count = int(input("How many groups? "))
-classrooms_count = int(input("How many classrooms? "))
+def main():
+    input_type = int(input("1 - Auto, 2 - Manual: "))
+    if input_type == 1:
+        groups = list(set([randint(1, 25) for i in range(randint(0, 9))]))
+        rooms = list(set([randint(5, 100) for i in range(randint(0, 9))]))
+        print(groups)
+        print(rooms)
+    elif input_type == 2:
+        groups = list(map(int, input("Groups sizes list (separate them by comma): ").replace(" ", "").split(",")))
+        rooms = list(map(int, input("Rooms sizes list (separate them by comma): ").replace(" ", "").split(",")))
+        print(groups)
+        print(rooms)
+    else:
+        print("Enter 1 or 2")
+        main()
 
-groups_sizes = [int(input("g%d: " % i)) for i in range(1, groups_count + 1)]
-classrooms_sizes = [int(input("c%d: " % i)) for i in range(1, classrooms_count + 1)]
+    groups_count = len(groups)
+    rooms_count = len(rooms)
 
-groups_names = ["g%d" % (x+1) for x in range(groups_count)]
-classrooms_names = ["s%d" % (x+1) for x in range(classrooms_count)]
-time_slots = int(input("How many time slots? "))
+    groups_names = ["group %d" % (i+1) for i in range(groups_count)]
+    rooms_names = ["room %d" % (i+1) for i in range(rooms_count)]
 
-groups = [g.add_vertex(Vertex(groups_names[i], groups_sizes[i])) for i in range(len(groups_names))]
-classrooms = [g.add_vertex(Vertex(classrooms_names[i], classrooms_sizes[i])) for i in range(len(classrooms_sizes))]
+    time_slots = int(input("How many time slots? "))
 
-for t in range(1, time_slots + 1):
-    pair_exists = True
-    while pair_exists:
-        if g.full_rooms and g.groups_to_add:
-            group_to_add = g.groups_to_add.pop()
-            max_room = max(g.full_rooms, key=lambda x: x.value)
-            if max_room.value >= group_to_add.value:
-                min_room = min_but_more_than(g.full_rooms, group_to_add.value)
-                g.full_rooms.remove(min_room)
-                print("Time: %d, group %s,%3d in room %s,%3d" %
-                      (t, group_to_add.name, group_to_add.value, min_room.name, min_room.value))
+    groups_to_add = {}
+    full_rooms = {}
+
+    [add_vertex(groups_names[i], groups[i], groups_to_add) for i in range(groups_count)]
+    del groups, groups_count
+    [add_vertex(rooms_names[i], rooms[i], full_rooms) for i in range(rooms_count)]
+
+    for t in range(1, time_slots + 1):
+        pair_exists = True
+        while pair_exists:
+            if full_rooms and groups_to_add:
+                group_to_add_name = groups_names[0]
+                group_to_add_size = groups_to_add.pop(groups_names[0])
+                del groups_names[0]
+                max_room_name = max(full_rooms, key=lambda x: full_rooms[x])
+                max_room_size = full_rooms[max_room_name]
+                if max_room_size >= group_to_add_size:
+                    min_room_name = min_but_more_than(full_rooms, group_to_add_size)
+                    print("Time: %d, %s,%3d in %s,%3d" %
+                          (t, group_to_add_name, group_to_add_size, min_room_name, full_rooms[min_room_name]))
+                    del full_rooms[min_room_name]
+                else:
+                    groups_to_add[group_to_add_name] = group_to_add_size
+            if full_rooms and groups_to_add:
+                max_room_size = max(full_rooms.values(), key=lambda x: x)
+                min_group_size = min(groups_to_add.values(), key=lambda x: x)
+                if max_room_size < min_group_size:
+                    pair_exists = False
             else:
-                g.groups_to_add.add(group_to_add)
-
-        if g.full_rooms and g.groups_to_add:
-            max_room = max(g.full_rooms, key=lambda x: x.value)
-            min_group = min(g.groups_to_add, key=lambda x: x.value)
-            if max_room.value < min_group.value:
                 pair_exists = False
-        else:
-            pair_exists = False
-    [g.add_vertex(Vertex(classrooms_names[i], classrooms_sizes[i])) for i in range(len(classrooms_sizes))]
+        [add_vertex(rooms_names[i], rooms[i], full_rooms) for i in range(rooms_count)]
+
+main()
